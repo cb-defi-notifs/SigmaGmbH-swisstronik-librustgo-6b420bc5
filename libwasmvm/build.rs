27eb72dc6bf4286d259b2c74d9f2d73b8ac5c680
Denis <dev-adv@swisstronik.com>
2022-10-13 16:51:50 +0400

    Added protobuf generation

--- libwasmvm/build.rs
+++ libwasmvm/build.rs
@@ -1,4 +1,8 @@
+extern crate protoc_rust;
+
 use std::env;
+use protoc_rust::Customize;
+
 
 fn main() {
     let crate_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
@@ -6,4 +10,15 @@ fn main() {
     cbindgen::generate(crate_dir)
         .expect("Unable to generate bindings")
         .write_to_file("bindings.h");
+
+    protoc_rust::Codegen::new()
+        .out_dir("src/protobuf_generated")
+        .includes(&["./protobuf_contracts"])
+        .inputs(&["./protobuf_contracts/ffi.proto"])
+        .customize(Customize {
+            gen_mod_rs: Some(true),
+            ..Default::default()
+        })
+        .run()
+        .expect("protoc");
 }
