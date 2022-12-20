62f9a9ff31ef78a37f8b7c54576f180f267bb010
MikkySnow <mike@swisstronik.com>
2022-12-20 13:28:55 +0100

    EVM backend now uses querier

--- libwasmvm/src/evm/mod.rs
+++ libwasmvm/src/evm/mod.rs
@@ -16,7 +16,7 @@ pub fn handle_transaction(querier: GoQuerier, data: ProtoTransactionData) -> Exe
     // Convert decoded protobuf data into TransactionData
     let tx = parse_protobuf_transaction_data(data);
     // Create FFI storage
-    let mut storage = crate::evm::storage::FFIStorage::new_mocked(querier);
+    let mut storage = crate::evm::storage::FFIStorage::new(querier);
     // Handle already parsed transaction and return execution result
     sgx_evm::handle_transaction_inner(tx, &mut storage)
 }
--- libwasmvm/src/evm/storage.rs
+++ libwasmvm/src/evm/storage.rs
@@ -8,23 +8,20 @@ use crate::querier::GoQuerier;
 /// This struct allows us to obtain state from keeper
 /// that is located outside of Rust code
 pub struct FFIStorage {
-    storage: BTreeMap<H160, BTreeMap<H256, H256>>,
-    contracts: BTreeMap<H160, Vec<u8>>,
-    accounts: BTreeMap<H160, Basic>,
     querier: GoQuerier,
 }
 
 impl Storage for FFIStorage {
     fn contains_key(&self, key: &H160) -> bool {
-        self.accounts.contains_key(key)
+        self.querier.query_contains_key(key)
     }
 
     fn get_account_storage_cell(&self, key: &H160, index: &H256) -> Option<H256> {
-        self.storage.get(key).and_then(|inner| inner.get(index)).copied()
+        self.querier.query_account_storage_cell(key, index)
     }
 
     fn get_account_code(&self, key: &H160) -> Option<Vec<u8>> {
-        self.contracts.get(key).cloned()
+        self.querier.query_account_code(key)
     }
 
     fn get_account(&self, key: &H160) -> Basic {
@@ -33,56 +30,37 @@ impl Storage for FFIStorage {
     }
 
     fn insert_account(&mut self, key: H160, data: Basic) {
-        self.accounts.insert(key, data);
+        self.querier.insert_account(key, data);
     }
 
     fn insert_account_code(&mut self, key: H160, code: Vec<u8>) {
-        self.contracts.insert(key, code);
+        self.querier.insert_account_code(key, code);
     }
 
     fn insert_storage_cell(&mut self, key: H160, index: H256, value: H256) {
-        self.storage.entry(key)
-            .and_modify(|inner| { inner.insert(index, value); })
-            .or_insert_with(|| {
-                let mut default = BTreeMap::new();
-                default.insert(index, value);
-                default
-            });
+        self.querier.insert_storage_cell(key, index, value);
     }
 
     fn remove(&mut self, key: &H160) {
-        self.accounts.remove(key);
+        self.querier.remove(key);
     }
 
     fn remove_account_code(&mut self, key: &H160) {
-        self.contracts.remove(key);
+        self.querier.remove_account_code(key);
     }
 
     fn remove_storage_cell(&mut self, key: &H160, index: &H256) {
-        self.storage.entry(*key).and_modify(|inner| { inner.remove(index); });
+        self.querier.remove_storage_cell(key, index);
     }
 
     fn remove_storage(&mut self, key: &H160) {
-        self.storage.remove(key);
+        self.querier.remove_storage(key);
     }
 }
 
 impl FFIStorage{
-    pub fn new_mocked(querier: GoQuerier) -> Self {
-        let mut accounts = BTreeMap::new();
-
-        accounts.insert(
-            H160::from_str("0x91e1f4bb1c1895f6c65cd8379de1323a8bf3cf7c").unwrap(),
-            Basic {
-                nonce: U256::zero(),
-                balance: U256::from_dec_str("1000000000000000000000").unwrap(),
-            }
-        );
-
+    pub fn new(querier: GoQuerier) -> Self {
         Self {
-            storage: BTreeMap::new(),
-            contracts: BTreeMap::new(),
-            accounts,
             querier
         }
     }
