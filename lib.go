171356cca9b4bc910bebe08100d61efd622a093b
MikkySnow <mike@swisstronik.com>
2023-03-02 18:48:10 +0100

    remove LibwasmvmVersion method

--- lib.go
+++ lib.go
@@ -101,10 +101,3 @@ func Create(
 
 	return executionResult, nil
 }
-
-// LibwasmvmVersion returns the version of the loaded library
-// at runtime. This can be used for debugging to verify the loaded version
-// matches the expected version.
-func LibwasmvmVersion() (string, error) {
-	return api.LibwasmvmVersion()
-}
