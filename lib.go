69449e3c1c9fd17882bd70f39290f70d394a04c9
MikkySnow <mike@swisstronik.com>
2023-03-06 14:23:18 +0100

    remove commented code and unused files

--- lib.go
+++ lib.go
@@ -7,24 +7,6 @@ import (
 	ffi "github.com/SigmaGmbH/librustgo/go_protobuf_gen"
 )
 
-//// Checksum represents a hash of the Wasm bytecode that serves as an ID. Must be generated from this library.
-//type Checksum []byte
-//
-//// WasmCode is an alias for raw bytes of the wasm compiled code
-//type WasmCode []byte
-
-//// KVStore is a reference to some sub-kvstore that is valid for one instance of a code
-//type KVStore = api.KVStore
-//
-//// GoAPI is a reference to some "precompiles", go callbacks
-//type GoAPI = api.GoAPI
-//
-//// Querier lets us make read-only queries on other modules
-//type Querier = types.Querier
-//
-//// GasMeter is a read-only version of the sdk gas meter
-//type GasMeter = api.GasMeter
-
 // Logs returned by EVM
 type Log = ffi.Log
 type Topic = ffi.Topic
