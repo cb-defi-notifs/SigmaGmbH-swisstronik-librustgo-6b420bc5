2ebd7e32b57e1f5f13c9867d1d45668d2173250b
MikkySnow <mike@swisstronik.com>
2023-03-31 00:32:34 +0200

    fix compilator warnings

--- sgx-wrapper/src/errors/rust.rs
+++ sgx-wrapper/src/errors/rust.rs
@@ -7,21 +7,10 @@ use crate::memory::UnmanagedVector;
 
 #[derive(Error, Debug)]
 pub enum RustError {
-    #[error("Empty argument: {}", name)]
-    EmptyArg {
-        name: String,
-        #[cfg(feature = "backtraces")]
-        backtrace: Backtrace,
-    },
     /// Whenever UTF-8 bytes cannot be decoded into a unicode string, e.g. in String::from_utf8 or str::from_utf8.
     #[error("Cannot decode UTF8 bytes into string: {}", msg)]
     InvalidUtf8 {
         msg: String,
-        #[cfg(feature = "backtraces")]
-        backtrace: Backtrace,
-    },
-    #[error("Ran out of gas")]
-    OutOfGas {
         #[cfg(feature = "backtraces")]
         backtrace: Backtrace,
     },
@@ -42,38 +31,11 @@ pub enum RustError {
         #[cfg(feature = "backtraces")]
         backtrace: Backtrace,
     },
-    #[error("Error decoding protobuf: {}", msg)]
-    ProtobufDecodeError {
-        msg: String,
-        #[cfg(feature = "backtraces")]
-        backtrace: Backtrace,
-    },
-    #[error("Enclave failure: {}", msg)]
-    EnclaveError {
-        msg: String,
-    }
-
 }
 
 impl RustError {
-    pub fn empty_arg<T: Into<String>>(name: T) -> Self {
-        RustError::EmptyArg {
-            name: name.into(),
-            #[cfg(feature = "backtraces")]
-            backtrace: Backtrace::capture(),
-        }
-    }
-
     pub fn invalid_utf8<S: ToString>(msg: S) -> Self {
         RustError::InvalidUtf8 {
-            msg: msg.to_string(),
-            #[cfg(feature = "backtraces")]
-            backtrace: Backtrace::capture(),
-        }
-    }
-
-    pub fn protobuf_decode<S: ToString>(msg: S) -> Self {
-        RustError::ProtobufDecodeError {
             msg: msg.to_string(),
             #[cfg(feature = "backtraces")]
             backtrace: Backtrace::capture(),
@@ -102,17 +64,6 @@ impl RustError {
             backtrace: Backtrace::capture(),
         }
     }
-
-    pub fn out_of_gas() -> Self {
-        RustError::OutOfGas {
-            #[cfg(feature = "backtraces")]
-            backtrace: Backtrace::capture(),
-        }
-    }
-
-    pub fn enclave_error<S: ToString>(msg: S) -> Self {
-        RustError::EnclaveError { msg: msg.to_string() }
-    }
 }
 
 
@@ -133,7 +84,6 @@ impl From<std::string::FromUtf8Error> for RustError {
 enum ErrnoValue {
     Success = 0,
     Other = 1,
-    OutOfGas = 2,
 }
 
 pub fn clear_error() {
@@ -157,53 +107,9 @@ pub fn set_error(err: RustError, error_msg: Option<&mut UnmanagedVector>) {
     }
 
     let errno = match err {
-        RustError::OutOfGas { .. } => ErrnoValue::OutOfGas,
         _ => ErrnoValue::Other,
     } as i32;
     set_errno(Errno(errno));
-}
-
-/// If `result` is Ok, this returns the Ok value and clears [errno].
-/// Otherwise it returns a null pointer, writes the error message to `error_msg` and sets [errno].
-///
-/// [errno]: https://utcc.utoronto.ca/~cks/space/blog/programming/GoCgoErrorReturns
-pub fn handle_c_error_ptr<T>(
-    result: Result<*mut T, RustError>,
-    error_msg: Option<&mut UnmanagedVector>,
-) -> *mut T {
-    match result {
-        Ok(value) => {
-            clear_error();
-            value
-        }
-        Err(error) => {
-            set_error(error, error_msg);
-            std::ptr::null_mut()
-        }
-    }
-}
-
-/// If `result` is Ok, this returns the binary representation of the Ok value and clears [errno].
-/// Otherwise it returns an empty vector, writes the error message to `error_msg` and sets [errno].
-///
-/// [errno]: https://utcc.utoronto.ca/~cks/space/blog/programming/GoCgoErrorReturns
-pub fn handle_c_error_binary<T>(
-    result: Result<T, RustError>,
-    error_msg: Option<&mut UnmanagedVector>,
-) -> Vec<u8>
-where
-    T: Into<Vec<u8>>,
-{
-    match result {
-        Ok(value) => {
-            clear_error();
-            value.into()
-        }
-        Err(error) => {
-            set_error(error, error_msg);
-            Vec::new()
-        }
-    }
 }
 
 /// If `result` is Ok, this returns the Ok value and clears [errno].
@@ -234,17 +140,6 @@ mod tests {
     use super::*;
     use errno::errno;
     use std::str;
-
-    #[test]
-    fn empty_arg_works() {
-        let error = RustError::empty_arg("gas");
-        match error {
-            RustError::EmptyArg { name, .. } => {
-                assert_eq!(name, "gas");
-            }
-            _ => panic!("expect different error"),
-        }
-    }
 
     #[test]
     fn invalid_utf8_works_for_strings() {
@@ -326,113 +221,6 @@ mod tests {
             }
             _ => panic!("expect different error"),
         }
-    }
-
-    #[test]
-    fn handle_c_error_binary_works() {
-        // Ok (non-empty vector)
-        let mut error_msg = UnmanagedVector::default();
-        let res: Result<Vec<u8>, RustError> = Ok(vec![0xF0, 0x0B, 0xAA]);
-        let data = handle_c_error_binary(res, Some(&mut error_msg));
-        assert_eq!(errno().0, ErrnoValue::Success as i32);
-        assert!(error_msg.is_none());
-        assert_eq!(data, vec![0xF0, 0x0B, 0xAA]);
-        let _ = error_msg.consume();
-
-        // Ok (empty vector)
-        let mut error_msg = UnmanagedVector::default();
-        let res: Result<Vec<u8>, RustError> = Ok(vec![]);
-        let data = handle_c_error_binary(res, Some(&mut error_msg));
-        assert_eq!(errno().0, ErrnoValue::Success as i32);
-        assert!(error_msg.is_none());
-        assert_eq!(data, Vec::<u8>::new());
-        let _ = error_msg.consume();
-
-        // Ok (non-empty slice)
-        let mut error_msg = UnmanagedVector::default();
-        let res: Result<&[u8], RustError> = Ok(b"foobar");
-        let data = handle_c_error_binary(res, Some(&mut error_msg));
-        assert_eq!(errno().0, ErrnoValue::Success as i32);
-        assert!(error_msg.is_none());
-        assert_eq!(data, Vec::<u8>::from(b"foobar" as &[u8]));
-        let _ = error_msg.consume();
-
-        // Ok (empty slice)
-        let mut error_msg = UnmanagedVector::default();
-        let res: Result<&[u8], RustError> = Ok(b"");
-        let data = handle_c_error_binary(res, Some(&mut error_msg));
-        assert_eq!(errno().0, ErrnoValue::Success as i32);
-        assert!(error_msg.is_none());
-        assert_eq!(data, Vec::<u8>::new());
-        let _ = error_msg.consume();
-
-        // Ok (checksum)
-        // let mut error_msg = UnmanagedVector::default();
-        // let res: Result<Checksum, RustError> = Ok(Checksum::from([
-        //     0x72, 0x2c, 0x8c, 0x99, 0x3f, 0xd7, 0x5a, 0x76, 0x27, 0xd6, 0x9e, 0xd9, 0x41, 0x34,
-        //     0x4f, 0xe2, 0xa1, 0x42, 0x3a, 0x3e, 0x75, 0xef, 0xd3, 0xe6, 0x77, 0x8a, 0x14, 0x28,
-        //     0x84, 0x22, 0x71, 0x04,
-        // ]));
-        // let data = handle_c_error_binary(res, Some(&mut error_msg));
-        // assert_eq!(errno().0, ErrnoValue::Success as i32);
-        // assert!(error_msg.is_none());
-        // assert_eq!(
-        //     data,
-        //     vec![
-        //         0x72, 0x2c, 0x8c, 0x99, 0x3f, 0xd7, 0x5a, 0x76, 0x27, 0xd6, 0x9e, 0xd9, 0x41, 0x34,
-        //         0x4f, 0xe2, 0xa1, 0x42, 0x3a, 0x3e, 0x75, 0xef, 0xd3, 0xe6, 0x77, 0x8a, 0x14, 0x28,
-        //         0x84, 0x22, 0x71, 0x04,
-        //     ]
-        // );
-        // let _ = error_msg.consume();
-
-        // Err (vector)
-        let mut error_msg = UnmanagedVector::default();
-        let res: Result<Vec<u8>, RustError> = Err(RustError::panic());
-        let data = handle_c_error_binary(res, Some(&mut error_msg));
-        assert_eq!(errno().0, ErrnoValue::Other as i32);
-        assert!(error_msg.is_some());
-        assert_eq!(data, Vec::<u8>::new());
-        let _ = error_msg.consume();
-
-        // Err (slice)
-        let mut error_msg = UnmanagedVector::default();
-        let res: Result<&[u8], RustError> = Err(RustError::panic());
-        let data = handle_c_error_binary(res, Some(&mut error_msg));
-        assert_eq!(errno().0, ErrnoValue::Other as i32);
-        assert!(error_msg.is_some());
-        assert_eq!(data, Vec::<u8>::new());
-        let _ = error_msg.consume();
-
-        // Err (checksum)
-        // let mut error_msg = UnmanagedVector::default();
-        // let res: Result<Checksum, RustError> = Err(RustError::panic());
-        // let data = handle_c_error_binary(res, Some(&mut error_msg));
-        // assert_eq!(errno().0, ErrnoValue::Other as i32);
-        // assert!(error_msg.is_some());
-        // assert_eq!(data, Vec::<u8>::new());
-        // let _ = error_msg.consume();
-    }
-
-    #[test]
-    fn handle_c_error_binary_clears_an_old_error() {
-        // Err
-        let mut error_msg = UnmanagedVector::default();
-        let res: Result<Vec<u8>, RustError> = Err(RustError::panic());
-        let data = handle_c_error_binary(res, Some(&mut error_msg));
-        assert_eq!(errno().0, ErrnoValue::Other as i32);
-        assert!(error_msg.is_some());
-        assert_eq!(data, Vec::<u8>::new());
-        let _ = error_msg.consume();
-
-        // Ok
-        let mut error_msg = UnmanagedVector::default();
-        let res: Result<Vec<u8>, RustError> = Ok(vec![0xF0, 0x0B, 0xAA]);
-        let data = handle_c_error_binary(res, Some(&mut error_msg));
-        assert_eq!(errno().0, ErrnoValue::Success as i32);
-        assert!(error_msg.is_none());
-        assert_eq!(data, vec![0xF0, 0x0B, 0xAA]);
-        let _ = error_msg.consume();
     }
 
     #[test]
