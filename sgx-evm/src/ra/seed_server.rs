9a9df6b4a0a6cc8c72da43d2b56a17250a39eb02
MikkySnow <mike@swisstronik.com>
2023-04-05 14:54:00 +0000

    reformat code

--- sgx-evm/src/ra/seed_server.rs
+++ sgx-evm/src/ra/seed_server.rs
@@ -5,6 +5,7 @@ use sgx_types::*;
 use std::backtrace::{self, PrintFormat};
 
 use itertools::Itertools;
+use rustls;
 use std::io;
 use std::io::{Read, Write};
 use std::net::TcpStream;
@@ -22,55 +23,71 @@ struct ClientAuth {
 
 impl ClientAuth {
     fn new(outdated_ok: bool) -> ClientAuth {
-        ClientAuth{ outdated_ok : outdated_ok }
+        ClientAuth {
+            outdated_ok: outdated_ok,
+        }
     }
 }
 
 impl rustls::ClientCertVerifier for ClientAuth {
-    fn client_auth_root_subjects(&self, _sni: Option<&webpki::DNSName>) -> Option<rustls::DistinguishedNames> {
+    fn client_auth_root_subjects(
+        &self,
+        _sni: Option<&webpki::DNSName>,
+    ) -> Option<rustls::DistinguishedNames> {
         Some(rustls::DistinguishedNames::new())
     }
 
-    fn verify_client_cert(&self, _certs: &[rustls::Certificate], _sni: Option<&webpki::DNSName>)
-    -> Result<rustls::ClientCertVerified, rustls::TLSError> {
+    fn verify_client_cert(
+        &self,
+        _certs: &[rustls::Certificate],
+        _sni: Option<&webpki::DNSName>,
+    ) -> Result<rustls::ClientCertVerified, rustls::TLSError> {
         println!("client cert: {:?}", _certs);
-            // This call will automatically verify cert is properly signed
-            match super::cert::verify_mra_cert(&_certs[0].0) {
-                Ok(()) => {
+        // This call will automatically verify cert is properly signed
+        match super::cert::verify_mra_cert(&_certs[0].0) {
+            Ok(()) => {
+                return Ok(rustls::ClientCertVerified::assertion());
+            }
+            Err(sgx_status_t::SGX_ERROR_UPDATE_NEEDED) => {
+                if self.outdated_ok {
+                    println!("outdated_ok is set, overriding outdated error");
                     return Ok(rustls::ClientCertVerified::assertion());
-                }
-                Err(sgx_status_t::SGX_ERROR_UPDATE_NEEDED) => {
-                    if self.outdated_ok {
-                        println!("outdated_ok is set, overriding outdated error");
-                        return Ok(rustls::ClientCertVerified::assertion());
-                    } else {
-                        return Err(rustls::TLSError::WebPKIError(webpki::Error::ExtensionValueInvalid));
-                    }
-                }
-                Err(_) => {
-                    return Err(rustls::TLSError::WebPKIError(webpki::Error::ExtensionValueInvalid));
+                } else {
+                    return Err(rustls::TLSError::WebPKIError(
+                        webpki::Error::ExtensionValueInvalid,
+                    ));
                 }
             }
+            Err(_) => {
+                return Err(rustls::TLSError::WebPKIError(
+                    webpki::Error::ExtensionValueInvalid,
+                ));
+            }
+        }
     }
 }
 
 struct ServerAuth {
-    outdated_ok: bool
+    outdated_ok: bool,
 }
 
 impl ServerAuth {
     fn new(outdated_ok: bool) -> ServerAuth {
-        ServerAuth{ outdated_ok : outdated_ok }
+        ServerAuth {
+            outdated_ok: outdated_ok,
+        }
     }
 }
 
 impl rustls::ServerCertVerifier for ServerAuth {
-    fn verify_server_cert(&self,
-              _roots: &rustls::RootCertStore,
-              _certs: &[rustls::Certificate],
-              _hostname: webpki::DNSNameRef,
-              _ocsp: &[u8]) -> Result<rustls::ServerCertVerified, rustls::TLSError> {
-    println!("server cert: {:?}", _certs);
+    fn verify_server_cert(
+        &self,
+        _roots: &rustls::RootCertStore,
+        _certs: &[rustls::Certificate],
+        _hostname: webpki::DNSNameRef,
+        _ocsp: &[u8],
+    ) -> Result<rustls::ServerCertVerified, rustls::TLSError> {
+        println!("server cert: {:?}", _certs);
         // This call will automatically verify cert is properly signed
         match super::cert::verify_mra_cert(&_certs[0].0) {
             Ok(()) => {
@@ -81,11 +98,15 @@ impl rustls::ServerCertVerifier for ServerAuth {
                     println!("outdated_ok is set, overriding outdated error");
                     return Ok(rustls::ServerCertVerified::assertion());
                 } else {
-                    return Err(rustls::TLSError::WebPKIError(webpki::Error::ExtensionValueInvalid));
+                    return Err(rustls::TLSError::WebPKIError(
+                        webpki::Error::ExtensionValueInvalid,
+                    ));
                 }
             }
             Err(_) => {
-                return Err(rustls::TLSError::WebPKIError(webpki::Error::ExtensionValueInvalid));
+                return Err(rustls::TLSError::WebPKIError(
+                    webpki::Error::ExtensionValueInvalid,
+                ));
             }
         }
     }
@@ -93,17 +114,18 @@ impl rustls::ServerCertVerifier for ServerAuth {
 
 #[no_mangle]
 pub unsafe extern "C" fn ecall_start_seed_server(
-    socket_fd: c_int, 
-    sign_type: sgx_quote_sign_type_t
+    socket_fd: c_int,
+    sign_type: sgx_quote_sign_type_t,
 ) {
     println!("Starting seed server...");
-    
+
     // Generate Keypair
     let ecc_handle = SgxEccHandle::new();
     let _result = ecc_handle.open();
     let (prv_k, pub_k) = ecc_handle.create_key_pair().unwrap();
 
-    let (attn_report, sig, cert) = match super::utils::create_attestation_report(&pub_k, sign_type) {
+    let (attn_report, sig, cert) = match super::utils::create_attestation_report(&pub_k, sign_type)
+    {
         Ok(r) => r,
         Err(e) => {
             println!("Error in create_attestation_report: {:?}", e);
@@ -112,7 +134,8 @@ pub unsafe extern "C" fn ecall_start_seed_server(
     };
 
     let payload = attn_report + "|" + &sig + "|" + &cert;
-    let (key_der, cert_der) = match super::cert::gen_ecc_cert(payload, &prv_k, &pub_k, &ecc_handle) {
+    let (key_der, cert_der) = match super::cert::gen_ecc_cert(payload, &prv_k, &pub_k, &ecc_handle)
+    {
         Ok(r) => r,
         Err(e) => {
             println!("Error in gen_ecc_cert: {:?}", e);
@@ -121,19 +144,19 @@ pub unsafe extern "C" fn ecall_start_seed_server(
     };
     let _result = ecc_handle.close();
 
-
     let mut cfg = rustls::ServerConfig::new(Arc::new(ClientAuth::new(true)));
     let mut certs = Vec::new();
     certs.push(rustls::Certificate(cert_der));
     let privkey = rustls::PrivateKey(key_der);
 
-    cfg.set_single_cert_with_ocsp_and_sct(certs, privkey, vec![], vec![]).unwrap();
+    cfg.set_single_cert_with_ocsp_and_sct(certs, privkey, vec![], vec![])
+        .unwrap();
 
     let mut sess = rustls::ServerSession::new(&Arc::new(cfg));
     let mut conn = TcpStream::new(socket_fd).unwrap();
 
     let mut tls = rustls::Stream::new(&mut sess, &mut conn);
-    let mut plaintext = [0u8;1024]; //Vec::new();
+    let mut plaintext = [0u8; 1024]; //Vec::new();
     match tls.read(&mut plaintext) {
         Ok(_) => println!("Client said: {}", str::from_utf8(&plaintext).unwrap()),
         Err(e) => {
