58e906eb233897abfbd3c27c6017ea4a11028d27
MikkySnow <mike@swisstronik.com>
2023-04-05 11:39:55 +0000

    chore

--- sgx-evm/src/ra/mod.rs
+++ sgx-evm/src/ra/mod.rs
@@ -1,5 +1,6 @@
 
 use sgx_types::*;
+use std::slice;
 
 pub mod keychain;
 
@@ -24,5 +25,9 @@ pub unsafe extern "C" fn ecall_init_node() -> sgx_status_t {
 /// Creates attestation certificate and saves it to unprotected memory
 pub unsafe extern "C" fn ecall_create_report(api_key: *const u8) -> sgx_status_t {
     println!("Creating attestation report");
+
+    // Read API key for IAS from slice
+    let api_key_slice = slice::from_raw_parts(api_key, 32usize);
+
     sgx_status_t::SGX_SUCCESS
 }
