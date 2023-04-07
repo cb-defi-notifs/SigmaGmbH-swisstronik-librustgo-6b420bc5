a7cba07448b594f0c5abb4276a34d6e91eb90bb0
MikkySnow <mike@swisstronik.com>
2023-04-07 11:50:31 +0000

    update intel endpoints

--- sgx-evm/src/ra/mod.rs
+++ sgx-evm/src/ra/mod.rs
@@ -9,8 +9,8 @@ pub mod seed_client;
 pub mod utils;
 
 pub const DEV_HOSTNAME: &'static str = "api.trustedservices.intel.com";
-pub const SIGRL_SUFFIX: &'static str = "/sgx/dev/attestation/v3/sigrl/";
-pub const REPORT_SUFFIX: &'static str = "/sgx/dev/attestation/v3/report";
+pub const SIGRL_SUFFIX: &'static str = "/sgx/dev/attestation/v4/sigrl/";
+pub const REPORT_SUFFIX: &'static str = "/sgx/dev/attestation/v4/report";
 pub const CERTEXPIRYDAYS: i64 = 90i64;
 
 #[no_mangle]
