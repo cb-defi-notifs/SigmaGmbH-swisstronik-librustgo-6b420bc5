7a57a124ea4758223290359a17fe8e7acca0e88b
MikkySnow <mike@swisstronik.com>
2023-04-12 15:54:44 +0000

    add registration key impl

--- sgx-evm/src/key_manager.rs
+++ sgx-evm/src/key_manager.rs
@@ -3,6 +3,8 @@ use sgx_tstd::sgxfs::SgxFile;
 use sgx_types::{sgx_read_rand, sgx_status_t, SgxResult};
 use std::io::{Read, Write};
 
+
+pub const REGISTRATION_KEY_SIZE: usize = 32;
 pub const SEED_SIZE: usize = 32;
 pub const SEED_FILENAME: &str = ".swtr_seed";
 
@@ -85,5 +87,45 @@ impl KeyManager {
         };
 
         Ok(Self { master_key })
+    }
+}
+
+/// RegistrationKey handles all operations with registration key such as derivation of public key,
+/// derivation of encryption key, etc.
+pub struct RegistrationKey {
+    inner: [u8; REG_SIZE]
+}
+
+impl RegistrationKey {
+    /// Generates public key for seed sharing
+    pub fn public_key(&self) -> x25519_dalek::PublicKey {
+        let secret = x25519_dalek::StaticSecret::from(self.inner);
+        x25519_dalek::PublicKey::from(&secret)
+    }
+
+    /// Generates random registration key
+    pub fn random() -> SgxResult<Self> {
+        // Generate random seed
+        let mut buffer = [0u8; REGISTRATION_KEY_SIZE];
+        let res = unsafe {
+            sgx_read_rand(&mut buffer as *mut u8, REGISTRATION_KEY_SIZE)
+        };
+
+        match res {
+            sgx_status_t::SGX_SUCCESS => {
+                return Ok(Self {inner: buffer})
+            },
+            _ => {
+                println!("[KeyManager] Cannot generate random registration key. Reason: {:?}", res.as_str());
+                return Err(res);
+            }
+        }
+    }
+
+    /// Performes Diffie-Hellman derivation of encryption key for seed encryption
+    /// * public_key – User public key
+    pub fn diffie_hellman(&self, public_key: x25519_dalek::PublicKey) -> x25519_dalek::SharedSecret {
+        let secret = x25519_dalek::StaticSecret::from(self.inner);
+        secret.diffie_hellman(&public_key)
     }
 }
