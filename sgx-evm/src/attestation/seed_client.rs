a6c1fe6eb32e7b77f01d14ed48261bf476aef209
MikkySnow <mike@swisstronik.com>
2023-04-17 11:30:25 +0000

    update client side

--- sgx-evm/src/attestation/seed_client.rs
+++ sgx-evm/src/attestation/seed_client.rs
@@ -140,18 +140,54 @@ fn request_seed_inner(socket_fd: c_int) -> sgx_status_t {
     }
 
     let mut plaintext = Vec::new();
-    match conn.read_to_end(&mut plaintext) {
-        Ok(_) => {
-            // TODO: Server should return encrypted seed
-            println!("Server replied: {}", str::from_utf8(&plaintext).unwrap());
-        }
+    match tls.read_to_end(&mut plaintext) {
         Err(ref err) if err.kind() == io::ErrorKind::ConnectionAborted => {
-            println!("EOF (tls)");
+            println!("[Enclave] Seed Client: connection aborted");
+            return sgx_status_t::SGX_ERROR_UNEXPECTED;
         }
-        Err(e) => println!("Error in read_to_end: {:?}", e),
+        Err(e) => {
+            println!("[Enclave] Seed Client: error in read_to_end: {:?}", e);
+            return sgx_status_t::SGX_ERROR_UNEXPECTED;
+        }
+        _ => {}
+    };
+
+    // Check size of response. It should be equal or more 90 bytes
+    // 32 public key | 16 nonce | ciphertext
+    if plaintext.len() < ENCRYPTED_KEY_SIZE {
+        println!("[Enclave] Seed Client: wrong response size");
+        return sgx_status_t::SGX_ERROR_UNEXPECTED;
     }
 
-    // TODO: Decrypt seed and seal it
+    // Extract public key and nonce + ciphertext
+    let public_key = &plaintext[..PUBLIC_KEY_SIZE];
+    let encrypted_seed = &plaintext[PUBLIC_KEY_SIZE..];
+
+    // Construct key manager
+    let key_manager = KeyManager::from_encrypted_seed(
+        &registration_key,
+        public_key.to_vec(),
+        encrypted_seed.to_vec(),
+    );
+    let key_manager = match key_manager {
+        Ok(key_manager) => key_manager,
+        Err(err) => {
+            println!(
+                "[Enclave] Seed Client: cannot construct key manager. Reason: {:?}",
+                err
+            );
+            return sgx_status_t::SGX_ERROR_UNEXPECTED;
+        }
+    };
+
+    // Seal master key
+    if let Err(error_status) = key_manager.seal() {
+        println!(
+            "[Enclave] Seed Client: cannot seal master key. Reason: {:?}",
+            error_status.as_str()
+        );
+        return error_status;
+    }
 
     sgx_status_t::SGX_SUCCESS
 }
